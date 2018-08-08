c6407af3ef88cd666101e41d75f07c07b2d7ebae
Gibran Khrisna Putra <gibrankhrisnaputra@gmail.com>
2018-08-08 16:09:45 +0700

    Add email column & widen form

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,6 +8,7 @@ const inputs = form.querySelectorAll('input')
 const inputName = document.getElementsByName('name')[0]
 const inputPhoneNumber = document.getElementsByName('phone_number')[0]
 const inputAddress = document.getElementsByName('address')[0]
+const inputEmail = document.getElementsByName('email')[0]
 const buttonSubmit = form.querySelector('button')
 
 renderAddresses(getAddresses())
@@ -26,7 +27,7 @@ search.addEventListener('input', function (e) {
 	renderAddresses(filteredAddress)
 })
 
-inputAddress.addEventListener('keyup', e => e.keyCode == 13 ? insertAddress() : false) // insert address on enter
+inputEmail.addEventListener('keyup', e => e.keyCode == 13 ? insertAddress() : false) // insert address on enter
 form.querySelector('button').addEventListener('click', insertAddress) // insert address
 buttonForm.addEventListener('click', showForm) // show form
 overlay.querySelector('.close').addEventListener('click', hideForm) // hide form
@@ -46,14 +47,15 @@ function insertAddress () {
   const name = inputName.value
   const phoneNumber = inputPhoneNumber.value
   const address = inputAddress.value
+  const email = inputEmail.value
 
-  if (inputName.value !== '' || inputPhoneNumber.value !== '' || inputAddress.value !== '') {
+  if (inputName.value !== '' || inputPhoneNumber.value !== '' || inputAddress.value !== '' || inputEmail.value !== '') {
     const addresses = getAddresses()
-    const newAddress = { name, phoneNumber, address }
+    const newAddress = { name, phoneNumber, address, email }
 
     window.localStorage.setItem('addresses', JSON.stringify({ addresses: [...addresses, newAddress]}))
 
-    appendAddress(getAddresses().length, name, phoneNumber, address)
+    appendAddress(getAddresses().length, name, phoneNumber, address, email)
 
     hideForm()
   }
@@ -65,22 +67,25 @@ function renderAddresses (addresses) {
 	}
 }
 
-function appendAddress (number, name, phoneNumber, address) {
+function appendAddress (number, name, phoneNumber, address, email) {
   const row = document.createElement('tr')
   const numberData = document.createElement('td')
   const nameData = document.createElement('td')
   const phoneNumberData = document.createElement('td')
   const addressData = document.createElement('td')
+  const emailData = document.createElement('td')
 
   numberData.innerText = number
   phoneNumberData.innerText = phoneNumber
   nameData.innerText = name
   addressData.innerText = address
+  emailData.innerText = email
 
   row.appendChild(numberData)
   row.appendChild(nameData)
   row.appendChild(phoneNumberData)
   row.appendChild(addressData)
+  row.appendChild(emailData)
 
   tbody.appendChild(row)
 }
